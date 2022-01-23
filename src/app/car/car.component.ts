632fdb249886d8b6e4fa06ad8a266d224193fd78
Mikhail Prokudin <mirex.doors@mail.ru>
2022-01-23 21:32:26 +0700

    one-way binding

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.sass']
 })
-export class CarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CarComponent {
+  carName:String = "Ford";
+  carYear:Number = 2017;
 }
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.sass']
 })
-export class CarsComponent implements OnInit {
+export class CarsComponent {
+  isDisabledAdding: boolean = true;
+  carAddingStatus: string = '';
+  inputText: string = '';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.isDisabledAdding = false;
+    }, 1000);
+  }
 
-  ngOnInit(): void {
+  addCar():void {
+    this.carAddingStatus = "Car added!";
+  }
+
+  onInput(event: Event) {
+    this.inputText = (<HTMLInputElement>event.target).value;
   }
 
 }
